a63f5385619fec4d1d73b8e059f1a9c3458cb2e1
Maksim Kropanov <kropanov2012@gmail.com>
2024-10-13 10:31:18 +0300

    feat: generate a new component - TaskComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { USERS } from './data/users';
+import { TaskComponent } from './task/task.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, HeaderComponent, UserComponent],
+    imports: [CommonModule, RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
